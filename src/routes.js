7291a7cca0ce625901757c84cdba5645ec295fdb
anderlfrias <anderl.friaas@gmail.com>
2022-03-23 09:59:09 -0400

    Creacion de las paginas basicas necesarias: home, login y registro

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Home from './routes/Home.svelte';
 import Login from './routes/Login.svelte';
+import Register from './routes/Register.svelte'
 
 export const routes = {
     '/': Home,
     '/login': Login,
+    '/registro': Register
 }
 
 export default routes;
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,9 +1,9 @@
 <script>
-    import Layout from "../components/Layout.svelte";   
+    import Layout from "../components/Layout.svelte";
 </script>
 <Layout>
 	<div class="container-fluid flex-grow-1 container-p-y">
-	  <h1>Hello World</h1>
+		<h1>Hello World</h1>
 		<p>This is the Svelte app.</p>
 	</div>
 </Layout>
